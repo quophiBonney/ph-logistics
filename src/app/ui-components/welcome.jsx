0ed95a71e81170b385053a15b548c7e3694f4bb3
Solomon Bonney <quophiclef@gmail>
2023-12-29 21:06:25 +0000

    changed cargo image on home

--- src/app/ui-components/welcome.jsx
+++ src/app/ui-components/welcome.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import cargo from "../assets/cargo.jpg";
+import cargo from "../assets/cargo-container.jpg";
 import Link from "next/link";
 const Welcome = () => {
   return (
